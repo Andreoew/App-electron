8447fa7a104f2889a48383528a1f406b134def86
André Silva <andreoew@hotmail.com>
2022-06-11 01:46:43 -0300

    update

---
+++ README.md
@@ -0,0 +1,14 @@
+## App realizado com aulas da Roockseat
+
+## Features
+
+- :electron: **Electron** — desktop apps with JavaScript, HTML, and CSS
+- 🔺**Sentry** - cross platform application monitoring
+- 💖**Lint** — ESlint, Prettier and EditorConfig
+
+## Getting started
+
+1. Clone this repository;
+2. `cd app-electron`;<br />
+3. Run `yarn` to install dependencies.<br />
+4. Run `yarn start`.
--- main.js
+++ main.js
@@ -1,15 +1,117 @@
-const { resolve } = require('path');
-const { app, Menu, Tray } = require('electron');
+const { resolve, basename } = require('path');
+const {
+  app, Menu, Tray, dialog,
+} = require('electron');
 
-// app.dock.hide()
+const { spawn } = require('child_process');
+const fixPath = require('fix-path');
+const fs = require('fs');
 
-app.on('ready', () => {
-  const tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
+const Store = require('electron-store');
+const Sentry = require('@sentry/electron');
+
+fixPath();
+
+Sentry.init({ dsn: "https://79704556968347b88cfb7353ec853efe@o1284235.ingest.sentry.io/6494904" });
+
+const schema = {
+  projects: {
+    type: 'string',
+  },
+};
+
+let mainTray = {};
+
+if (app.dock) {
+  app.dock.hide();
+}
+
+const store = new Store({ schema });
+
+function getLocale() {
+  const locale = app.getLocale();
+
+  switch (locale) {
+    case 'es-419' || 'es':
+      return JSON.parse(fs.readFileSync(resolve(__dirname, 'locale/es.json')));
+    case 'pt-BR' || 'pt-PT':
+      return JSON.parse(fs.readFileSync(resolve(__dirname, 'locale/pt.json')));
+    default:
+      return JSON.parse(fs.readFileSync(resolve(__dirname, 'locale/en.json')));
+  }
+}
+
+function render(tray = mainTray) {
+  const storedProjects = store.get('projects');
+  const projects = storedProjects ? JSON.parse(storedProjects) : [];
+  const locale = getLocale();
+
+  const items = projects.map(({ name, path }) => ({
+    label: name,
+    submenu: [
+      {
+        label: locale.open,
+        click: () => {
+          spawn('code', [path], { shell: true });
+        },
+      },
+      {
+        label: locale.remove,
+        click: () => {
+          store.set('projects', JSON.stringify(projects.filter(item => item.path !== path)));
+          render();
+        },
+      },
+    ],
+  }));
 
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio', checked: true }
+    {
+      label: locale.add,
+      click: () => {
+        const result = dialog.showOpenDialog({ properties: ['openDirectory'] });
+
+        if (!result) return;
+
+        const [path] = result;
+        const name = basename(path);
+
+        store.set(
+          'projects',
+          JSON.stringify([
+            ...projects,
+            {
+              path,
+              name,
+            },
+          ]),
+        );
+
+        render();
+      },
+    },
+    {
+      type: 'separator',
+    },
+    ...items,
+    {
+      type: 'separator',
+    },
+    {
+      type: 'normal',
+      label: locale.close,
+      role: 'quit',
+      enabled: true,
+    },
   ]);
 
-  tray.setToolTip('This is my application');
   tray.setContextMenu(contextMenu);
-})
+
+  tray.on('click', tray.popUpContextMenu);
+}
+
+app.on('ready', () => {
+  mainTray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
+
+  render(mainTray);
+});
